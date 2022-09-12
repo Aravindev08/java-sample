ca4c61feff2d3e8473fa703ff23091651584e988
github_Praveen-webdev <praveen1598810@gmail.com>
2022-09-12 23:39:11 +0530

    first commit

---
+++ exercise1.java
@@ -0,0 +1,14 @@
+public class exercise1{
+    public static void main(String[] args){
+        int firstnum=63;
+        int secondnum=8;
+        int total=(firstnum+secondnum);
+        int total1=(firstnum-secondnum);
+        int total2=(firstnum*secondnum);
+        int total3=(firstnum%secondnum);
+        System.out.println(total);
+        System.out.println(total1);
+        System.out.println(total2);
+        System.out.println(total3);
+    }
+}
