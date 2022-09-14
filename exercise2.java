071dbe774e39deecc1decf1b65aca35c54441138
github_Praveen-webdev <praveen1598810@gmail.com>
2022-09-14 23:09:01 +0530

    just commit

---
+++ exercise2.java
@@ -0,0 +1,10 @@
+import java.nio.file.FileSystemAlreadyExistsException;
+
+public class exercise2 {
+    public static void main (String[] args){
+        float fst =2.5f;
+        float fst1=5.0f;
+        float ave =(fst + fst1);
+        System.out.println(ave);
+    }  
+}
